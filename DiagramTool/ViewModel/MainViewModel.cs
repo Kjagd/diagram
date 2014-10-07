b46a44d7c2731aa260514ee98659e7a33598da86
WhatDo <jonas.misc@gmail.com>
2014-10-07 10:56:11 +0200

    Ribbon and stuff bitches

---
+++ DiagramTool/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Diagram;
+
+namespace DiagramTool.ViewModel
+{
+
+    class MainViewModel : ViewModelBase
+    {
+
+        private ObservableCollection<Klass> klasses;
+    }
+}
