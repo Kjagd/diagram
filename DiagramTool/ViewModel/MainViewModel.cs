670b93c89bfff313b91e8afedbb6b5bbd8291b64
WhatDo <jonas.misc@gmail.com>
2014-10-07 11:12:38 +0200

    added mvvmlight

--- DiagramTool/ViewModel/MainViewModel.cs
+++ DiagramTool/ViewModel/MainViewModel.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Diagram;
+using GalaSoft.MvvmLight;
 
 namespace DiagramTool.ViewModel
 {
-
-    class MainViewModel : ViewModelBase
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
     {
-
-        private ObservableCollection<Klass> klasses;
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
     }
 }
