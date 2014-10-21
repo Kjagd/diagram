53700b57679fc8bd3c3af802feedd63e0f26f0f9
WhatDo <jonas.misc@gmail.com>
2014-10-21 11:52:31 +0200

    removed klassviewmodel

--- ClassLibrary1/Klass.cs
+++ ClassLibrary1/Klass.cs
@@ -9,25 +9,35 @@ namespace Diagram
 {
     public class Klass
     {
-        private string package;
-        public string Package { get { return package; } set { package = value; } }
+
+        // View properties
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public string Package { get; set; }
         public string Name { get; set; }
 
         ArrayList attributes = new ArrayList();
         ArrayList operations = new ArrayList();
-        
+
         // Visibility of attributes and operations
 
         public Klass(string name)
         {
             Name = name;
+            Width = 150;
+            Height = 150;
         }
 
-        public void addAttribute(string attribute) {
+        public void addAttribute(string attribute)
+        {
             attributes.Add(attribute);
         }
 
-        public void addOperations(string operation) {
+        public void addOperations(string operation)
+        {
             operations.Add(operation);
         }
 
--- DiagramTool/ViewModel/MainViewModel.cs
+++ DiagramTool/ViewModel/MainViewModel.cs
@@ -8,20 +8,20 @@ namespace DiagramTool.ViewModel
     public class MainViewModel : ViewModelBase
     {
 
-        public ObservableCollection<KlassViewModel> Klasses { get; set; }
+        public ObservableCollection<Klass> Klasses { get; set; }
         public ObservableCollection<Relation> Relations { get; set; } 
 
         public MainViewModel()
         {
-            Klasses = new ObservableCollection<KlassViewModel>();
-            var k = new KlassViewModel {X = 200, Y = 200, Klass = new Klass("Lel")};
+            Klasses = new ObservableCollection<Klass>();
+            var k = new Klass("lel") {X = 200, Y = 200};
             Klasses.Add(k);
 
-            var c = new KlassViewModel {Klass = new Klass("YourMom")};
+            var c = new Klass("You're mom");
             Klasses.Add(c);
  
             Relations = new ObservableCollection<Relation>();
-            var r = new Inheritance(k.Klass, c.Klass);
+            var r = new Inheritance(k, c);
             Relations.Add(r);
         }
     }
