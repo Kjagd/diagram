5a33424d67205482fd1082010a9113eb41d02879
kjagd <kristianjagd@gmail.com>
2014-11-04 12:59:53 +0100

    you want fields yes

---
+++ ClassLibrary1/Field.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram
+{
+
+    public class Field
+    {
+        public String FieldName { get; set; }
+        public String AccessModifier { get; set; }
+
+        public Field(string fieldName, string accessModifier)
+        {
+            FieldName = fieldName;
+            AccessModifier = accessModifier;
+        }
+    }
+}
