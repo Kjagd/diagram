465375290bdd14240be1fa425c60ade4cc3a3562
WhatDo <jonas.misc@gmail.com>
2014-11-04 11:22:43 +0100

    Clean up folder

---
+++ DiagramTool/ViewModel/NodeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Diagram;
+using GalaSoft.MvvmLight;
+
+namespace DiagramTool.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class NodeViewModel : ViewModelBase
+    {
+
+        public Klass MyKlass { get; set; }
+
+        public NodeViewModel()
+        {
+        }
+    }
+}
